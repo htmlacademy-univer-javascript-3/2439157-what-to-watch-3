5b87c4050de1c3b2ec9573acec9415563d02db20
annetannet <annetpeach@gmail.com>
2023-10-08 23:10:18 +0500

    add components

---
+++ src/components/app/App.tsx
@@ -0,0 +1,15 @@
+import MainPage from "../../pages/main-page/MainPage";
+
+type AppProps = {
+  promoFilmName: string;
+  promoFilmGenre: string;
+  promoFilmYear: number;
+}
+
+function App({ promoFilmName, promoFilmGenre, promoFilmYear }: AppProps): JSX.Element {
+  return (
+    <MainPage promoFilmName={promoFilmName} promoFilmGenre={promoFilmGenre} promoFilmYear={promoFilmYear} />
+  )
+}
+
+export default App;
