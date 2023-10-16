3eb60f1e714f7529bb5439597aa794886ffff392
annetannet <annetpeach@gmail.com>
2023-10-16 10:10:48 +0500

    rm comment

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
-// eslint-disable-next-line react-refresh/only-export-components
 const Setting = {
   PromoFilmName: 'The Grand Budapest Hotel',
   PromoFilmGenre: 'Drama',
